649a9f7586f40f8286dbb9e49acb25218398bdd1
Eliel Haouzi <eliel.haouzi@gmail.com>
2020-10-06 12:34:44 +0300

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shortcode.MixProject do
   def project do
     [
       app: :shortcode,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
